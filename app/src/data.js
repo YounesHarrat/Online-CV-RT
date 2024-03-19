40e8084feb499d6f2cdc3889592ce1f1d47117d6
Younes Harrat <harratanis@hotmail.fr>
2024-03-19 10:16:00 +0000

    +translations , wip react-chrono translations + dark mode, wip better darkmode styles

--- app/src/data.js
+++ app/src/data.js
@@ -17,7 +17,8 @@
  */
 
 
-export const data = [
+export const dataEN = [
+    
     {
         title: "2014",
         cardSubtitle: "Dunkirk - Lycée de l'Europe",
@@ -43,18 +44,60 @@ export const data = [
     },
     {
         title: "2020-22",
-        cardTitle: "CDA Master: Conception Developpement of Application",
+        cardTitle: "CDA Master: Conception and Developpment of Application",
         cardSubtitle:"Apprentice Front-End Angular Developper at Picomto and through M2I Tech Academy",
         cardDetailedText: "cf Professionnal experience",    
     },
     {
         title: "2023",
         cardTitle: "Junior FullStack Developper",
-        cardSubtitle:"Worked at Touchdown as a Junior FullStack with Svelte and Nodejs Frameworks",
+        cardSubtitle:"Worked at Touchdown as a Junior FullStack Developper with Svelte and Nodejs Frameworks",
         cardDetailedText: "cf Professionnal experience",    
     },
     {
         title: "2024",
         cardTitle: "Ready to be your next Collegue !"
     }
+];
+
+export const dataFR = [
+    {
+        title: "2014",
+        cardSubtitle: "Dunkerque - Lycée de l'Europe",
+        cardTitle:"Bac S Svt",
+        cardDetailedText: "Specialité Mathematique - Option Anglais - Mention Trés bien >16/20 de moyenne.",    
+    },
+    {
+        title: "milieu-2014 à 2016 ",
+        cardSubtitle: "Dunkerque - Lycée de l'Europe",
+        cardTitle:"CPGE: Classe Préparatoire aux grandes Ecoles : PTSI / PT",
+        cardDetailedText: "1ère et seconde année de mathématique supérieur, la fillière PTSI - PT se spécialise dans la science industrielle. J'ai choisi après réflexion de me réorienter vers une licence et un master de développement web" 
+    },
+    {
+        title: "2016",
+        cardSubtitle: "Lille - Euratechnologies - GlobalKnowledge",
+        cardTitle:"Java JEE Certification ",
+        cardDetailedText: "Formation d'une durée de 3 mois que j'ai choisi de faire pendant ma réorientation.",    
+    },
+    {
+        title: "July 2020",
+        cardTitle:"Obtention Licence STS Informatique",
+        cardSubtitle: "Calais - University of the Littoral Côte d'Opale ",
+    },
+    {
+        title: "2020-22",
+        cardTitle: "Master CDA: Conception et Développement d' Application",
+        cardSubtitle:"Alternant Développeur Front-End chez Picomto et l'école M2I Tech Academy",
+        cardDetailedText: "cf Expérience professionnelle",    
+    },
+    {
+        title: "2023",
+        cardTitle: "Développeur FullStack Junior",
+        cardSubtitle:"En poste chez Touchdown en tant que Junior FullStack Developper avec les frameworks Svelte et NodeJs",
+        cardDetailedText: "cf Expérience professionnelle",    
+    },
+    {
+        title: "2024",
+        cardTitle: "Prêt à rejoindre vos équipes !"
+    }   
 ]
